8c8366f641dd1d8bd1bca7fe1e8b692e2ea8df9c
Félixbimoga <atanganalorel@gmail.com>
2026-07-23 12:04:53 +0100

    update

--- src/main/java/com/gargotrust/gestion_achats_enligne/iam/service/AuthService.java
+++ src/main/java/com/gargotrust/gestion_achats_enligne/iam/service/AuthService.java
@@ -59,8 +59,8 @@ public class AuthService implements IAuthService {
                 .build();
         accountRepository.save(account);
 
-        Role role = roleRepository.findByName(Role.CLIENT)
-                .orElseThrow(() -> new RuntimeException("ROLE_CLIENT not found in DB"));
+        Role role = roleRepository.findByName(Role.ADMIN_TRANSITAIRE)
+                .orElseThrow(() -> new RuntimeException("ROLE_ADMIN_TRANSITAIRE not found in DB"));
         AccountRole accountRole = AccountRole.builder().account(account).role(role).build();
         account.getAccountRoles().add(accountRole);
         accountRepository.save(account);
